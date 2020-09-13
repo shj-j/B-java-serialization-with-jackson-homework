4b865342511915736d8f2a471a8409f501965e3f
shj-j <shang.jie@outlook.com>
2020-09-13 17:20:49 +0800

    problem 2 hobby, done

--- src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
+++ src/main/java/com/thoughtworks/capability/gtb/vo/PersonVo.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.capability.gtb.vo;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,6 @@ public class PersonVo {
   private String id;
   private Integer age;
   private String name;
-  @JsonIgnore
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   private String hobby;
 }
